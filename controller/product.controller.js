9f6778bef6897d8ff7c0769d478d44ca3fd5772b
Ravid-jam <Ravid.jam02@gmail.com>
2025-02-18 14:18:12 +0530

    fix: Message proper in Product controller.

--- controller/product.controller.js
+++ controller/product.controller.js
@@ -111,7 +111,13 @@ exports.getSingleProduct = async (req, res) => {
     }).populate("category subcategory pataCategory");
     if (!product) return res.status(404).json({ message: "Product not found" });
 
-    res.status(200).json(product);
+    res
+      .status(200)
+      .json({
+        data: product,
+        message: "Product fetch successfully",
+        status: true,
+      });
   } catch (err) {
     res
       .status(500)
